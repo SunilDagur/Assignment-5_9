c4cb7f5b6989eb6a1406b389553e25b22926a699
Sunil Dagur <sunildagur566@gmail.com>
2023-12-06 18:05:17 +0530

    Add project files.

---
+++ Assignment 5_9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            
+            PerformOperation();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Caught exception: {ex.Message}");
+
+           
+            RethrowException(ex);
+        }
+    }
+
+    static void PerformOperation()
+    {
+        try
+        {
+            
+            throw new InvalidOperationException("Something went wrong in PerformOperation");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Caught exception in PerformOperation: {ex.Message}");
+
+            
+            throw;
+        }
+    }
+
+    static void RethrowException(Exception ex)
+    {
+        try
+        {
+            
+            Console.WriteLine($"Handling rethrown exception: {ex.Message}");
+        }
+        catch (Exception)
+        {
+            
+            Console.WriteLine("Exception handling code here");
+        }
+    }
+}
